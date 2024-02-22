79a712e5422f7e9b3459a684f1cbe7d1f573d2c2
Yousra <nice.sora@hotmail.com>
2024-02-23 01:12:39 +1100

    Defined columns on Category model

--- models/Category.js
+++ models/Category.js
@@ -1,16 +1,30 @@
 const { Model, DataTypes } = require("sequelize");
-
+// import our database connection from config.js
 const sequelize = require("../config/connection.js");
 
+// Create a new Sequelize model (table) for Category
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Define fields/columns on model
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
+    // Link to database connection
     sequelize,
+    // Set to false to remove `created_at` and `updated_at` fields
     timestamps: false,
+    // Prevent sequelize from renaming the table
     freezeTableName: true,
     underscored: true,
     modelName: "category",
